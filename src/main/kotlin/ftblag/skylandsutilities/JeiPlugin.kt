14df8f8356e9a0165627c99bf8c26c751074b561
FTB_lag <amg1999@yandex.ru>
2018-08-05 02:57:19 +0300

    Update 1.0.3

--- src/main/kotlin/ftblag/skylandsutilities/JeiPlugin.kt
+++ src/main/kotlin/ftblag/skylandsutilities/JeiPlugin.kt
@@ -6,6 +6,7 @@ import exnihilocreatio.compatibility.jei.sieve.SieveRecipeCategory
 import mezz.jei.api.IModPlugin
 import mezz.jei.api.IModRegistry
 import mezz.jei.api.JEIPlugin
+import net.blay09.mods.excompressum.compat.jei.CompressedHammerRecipeCategory
 import net.minecraft.item.ItemStack
 
 @JEIPlugin
@@ -16,5 +17,7 @@ class JeiPlugin : IModPlugin {
         registry.addRecipeCatalyst(ItemStack(SkylandsUtilities.skyrootHammer), HammerRecipeCategory.UID)
         registry.addRecipeCatalyst(ItemStack(SkylandsUtilities.holystoneHammer), HammerRecipeCategory.UID)
         registry.addRecipeCatalyst(ItemStack(SkylandsUtilities.skyrootBarrel), CompostRecipeCategory.UID)
+        registry.addRecipeCatalyst(ItemStack(SkylandsUtilities.holystoneCompressedHammer), CompressedHammerRecipeCategory.UID)
+        registry.addRecipeCatalyst(ItemStack(SkylandsUtilities.skyrootCompressedHammer), CompressedHammerRecipeCategory.UID)
     }
 }
--- src/main/kotlin/ftblag/skylandsutilities/SkylandsUtilities.kt
+++ src/main/kotlin/ftblag/skylandsutilities/SkylandsUtilities.kt
@@ -2,13 +2,14 @@ package ftblag.skylandsutilities
 
 import com.gildedgames.aether.common.blocks.BlocksAether
 import com.gildedgames.aether.common.items.ItemsAether
-import com.gildedgames.orbis_api.core.GameRegistrar
 import exnihilocreatio.blocks.BlockBarrel
 import exnihilocreatio.blocks.BlockSieve
 import exnihilocreatio.items.tools.CrookBase
 import exnihilocreatio.items.tools.HammerBase
 import exnihilocreatio.util.Data
 import exnihilocreatio.util.IHasModel
+import net.blay09.mods.excompressum.item.ItemCompressedCrook
+import net.blay09.mods.excompressum.item.ItemCompressedHammer
 import net.minecraft.block.Block
 import net.minecraft.block.material.Material
 import net.minecraft.creativetab.CreativeTabs
@@ -17,19 +18,12 @@ import net.minecraft.item.ItemStack
 import net.minecraft.util.ResourceLocation
 import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.common.event.FMLInitializationEvent
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent
-import net.minecraftforge.fml.common.registry.ForgeRegistries
 import net.minecraftforge.fml.common.registry.GameRegistry
 import net.minecraftforge.fml.relauncher.Side
 import net.minecraftforge.fml.relauncher.SideOnly
-import net.minecraftforge.common.ForgeHooks.getRegistryName
-import net.minecraft.client.renderer.block.model.ModelResourceLocation
-import net.minecraftforge.client.model.ModelLoader
+import net.minecraft.item.ItemShears
 
-
-
-
-@Mod(modid = SkylandsUtilities.MODID, name = "Skylands Utilities", version = "@VERSION@", dependencies = "required-after:exnihilocreatio@[1.12-0.2.4,);required-after:aether@[1.12.2-1.0.8,);")
+@Mod(modid = SkylandsUtilities.MODID, name = "Skylands Utilities", version = "@VERSION@", dependencies = "required-after:exnihilocreatio;required-after:aether@[1.12.2-1.0.8,);required-after:excompressum;")
 object SkylandsUtilities {
     const val MODID = "skylandsutilities"
 
@@ -42,17 +36,53 @@ object SkylandsUtilities {
 
     val skyrootSieve : Block = BlockSieve().setUnlocalizedName("skyroot_sieve").setCreativeTab(tab)
     val skyrootCrook = CrookBase("skyroot_crook", 64).setCreativeTab(tab)
-    val holystoneСrook = CrookBase("holystone_crook", 128).setCreativeTab(tab)
+    val holystoneCrook = CrookBase("holystone_crook", 128).setCreativeTab(tab)
     val skyrootHammer = HammerBase("skyroot_hammer", 64, Item.ToolMaterial.WOOD).setCreativeTab(tab)
     val holystoneHammer = HammerBase("holystone_hammer", 128, Item.ToolMaterial.STONE).setCreativeTab(tab)
     val holystoneStick = HolyStick()
     val skyrootBarrel = BlockBarrel(0, Material.WOOD).setUnlocalizedName("skyroot_barrel")
+    val holystoneShears = CustShears("holystone_shears", 128)
+    val skyrootShears = CustShears("skyroot_shears", 64)
+    val holystoneCompressedCrook = CustComprCrook("holystone_compressed_crook", 1152)
+    val skyrootCompressedCrook = CustComprCrook("skyroot_compressed_crook", 576)
+    val holystoneCompressedHammer = CustomComprHammer("holystone_compressed_hammer", Item.ToolMaterial.STONE, 1152)
+    val skyrootCompressedHammer = CustomComprHammer("skyroot_compressed_hammer", Item.ToolMaterial.WOOD, 576)
 
-    class HolyStick : Item, IHasModel{
+    class HolyStick : Item, IHasModel {
         constructor() {
             setCreativeTab(tab)
             setRegistryName("holystone_stick")
             setUnlocalizedName("holystone_stick")
+            Data.ITEMS.add(this)
+        }
+    }
+
+    class CustShears : ItemShears, IHasModel {
+        constructor(name : String, dmg : Int) {
+            setCreativeTab(tab)
+            setRegistryName(name)
+            setUnlocalizedName(name)
+            setMaxDamage(dmg)
+            Data.ITEMS.add(this)
+        }
+    }
+
+    class CustComprCrook : ItemCompressedCrook, IHasModel {
+        constructor(name : String, dmg : Int) {
+            setCreativeTab(tab)
+            setRegistryName(name)
+            setUnlocalizedName(name)
+            setMaxDamage(dmg)
+            Data.ITEMS.add(this)
+        }
+    }
+
+    class CustomComprHammer : ItemCompressedHammer, IHasModel {
+        constructor(name : String, material : ToolMaterial, dmg : Int) : super(material, "") {
+            setCreativeTab(tab)
+            setRegistryName(name)
+            setUnlocalizedName(name)
+            setMaxDamage(dmg)
             Data.ITEMS.add(this)
         }
     }
@@ -72,7 +102,7 @@ object SkylandsUtilities {
         GameRegistry.addShapedRecipe(ResourceLocation(MODID, "sky_hammer"), null, ItemStack(skyrootHammer),
                 " X ", " YX", "Y  ", 'X', BlocksAether.skyroot_planks, 'Y', ItemsAether.skyroot_stick)
 
-        GameRegistry.addShapedRecipe(ResourceLocation(MODID, "holy_crook"), null, ItemStack(holystoneСrook),
+        GameRegistry.addShapedRecipe(ResourceLocation(MODID, "holy_crook"), null, ItemStack(holystoneCrook),
                 "XX ", " X ", " X ", 'X', holystoneStick)
 
         GameRegistry.addShapedRecipe(ResourceLocation(MODID, "holy_hammer"), null, ItemStack(holystoneHammer),
@@ -83,5 +113,23 @@ object SkylandsUtilities {
 
         GameRegistry.addShapedRecipe(ResourceLocation(MODID, "skyroot_barrel"), null, ItemStack(skyrootBarrel),
                 "P P", "P P", "PSP", 'P', BlocksAether.skyroot_planks, 'S', BlocksAether.skyroot_slab)
+
+        GameRegistry.addShapedRecipe(ResourceLocation(MODID, "holy_shears"), null, ItemStack(holystoneShears),
+                " X", "X ", 'X', BlocksAether.holystone)
+
+        GameRegistry.addShapedRecipe(ResourceLocation(MODID, "skyroot_shears"), null, ItemStack(skyrootShears),
+                " X", "X ", 'X', BlocksAether.skyroot_planks)
+
+        GameRegistry.addShapedRecipe(ResourceLocation(MODID, "holy_compr_crook"), null, ItemStack(holystoneCompressedCrook),
+                "XXX", "XXX", "XXX", 'X', holystoneCrook)
+
+        GameRegistry.addShapedRecipe(ResourceLocation(MODID, "skyroot_compr_crook"), null, ItemStack(skyrootCompressedCrook),
+                "XXX", "XXX", "XXX", 'X', skyrootCrook)
+
+        GameRegistry.addShapedRecipe(ResourceLocation(MODID, "holy_compr_hammer"), null, ItemStack(holystoneCompressedHammer),
+                "XXX", "XXX", "XXX", 'X', holystoneHammer)
+
+        GameRegistry.addShapedRecipe(ResourceLocation(MODID, "skyroot_compr_hammer"), null, ItemStack(skyrootCompressedHammer),
+                "XXX", "XXX", "XXX", 'X', skyrootHammer)
     }
 }
